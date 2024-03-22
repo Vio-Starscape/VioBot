d75811ca6faf038e886c00dcb62c707f20d4b06f
Eric Beaulne <eric.beaulne@ontariotechu.net>
2024-03-21 21:06:34 -0400

    feat: added .env.example file and various other changes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 import logging
 import os
+from datetime import datetime
 from discord.ext import commands
+from discord.ext import tasks
 from vio import VioDB
 from dotenv import load_dotenv
 load_dotenv(override=True)
@@ -16,6 +18,7 @@ class Vio(commands.Bot):
             intents=discord.Intents.default()
         )
         self.db = VioDB(db_uri, database)
+        self.up_time = datetime.now()
 
     async def on_ready(self):
         logger.info(f'Logged in as {self.user} (ID: {self.user.id})')
@@ -33,15 +36,47 @@ class Vio(commands.Bot):
         await self.load_extension("ext.Valuation")
 
         # Testing
-        # guild = discord.Object(id=971952765955895317)
+        # guild = discord.Object(id=os.getenv("MAIN_GUILD_ID"))
         # self.tree.copy_global_to(guild=guild)
         # self.tree.clear_commands(guild=guild)
         # await self.tree.sync(guild=guild)
         await self.tree.sync()
 
+    @tasks.loop(minutes=10)
+    async def update(self):
+        """Update task
+
+        This task will update the items and roblox users every 10 minutes.
+        This can also be used for other tasks such as undercut pings
+        """
+        self.items = await self.db.get_item_list()
+        self.roblox_users = await self.db.get_roblox_users()
+
+vio = Vio(os.getenv("MOTOR_URI"), os.getenv("DATABASE"))
+
+@vio.tree.command()
+async def stats(interaction: discord.Interaction):
+
+    embed = discord.Embed(
+        title="General Information",
+        description="Here is some general information regarding Vio. Created by `Meaning`",
+        color=discord.Color.blurple()
+    )
+    embed.add_field(
+        name="Uptime",
+        value="Given an image of your inventory, I will evaluate the value of your items."
+    )
+    embed.add_field(
+        name="Guilds",
+        value=len(vio.guilds)
+    )
+    embed.set_footer(
+        text="Version: 1.18b"
+    )
+    await interaction.response.send_message(embed=embed, ephemeral=True)
+
 
 if __name__ == "__main__":
-    vio = Vio(os.getenv("MOTOR_URI"), os.getenv("DATABASE"))
     vio.run(
         os.getenv("BOT_TOKEN"), 
         root_logger=True,
