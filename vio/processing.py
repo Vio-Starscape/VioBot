24f943a32d64d48bbfcd90ccbe8e44842ba0441e
Meaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2024-03-21 21:33:12 -0400

    Add .env.example file and update main.py (#4)
    * feat: added .env.example file and various other changes
    
    * Add main entry point in main.py

--- vio/processing.py
+++ vio/processing.py
@@ -1,10 +1,14 @@
 from PIL import Image
+import aiopytesseract.constants
 from scipy import stats
 from io import BytesIO
 import aiopytesseract
 import re
 import numpy as np
 import cv2
+
+# Needed to add custom languages
+# aiopytesseract.constants.TESSERACT_LANGUAGES.add("starlstm_final")
 
 CONFIG = {
     "name": (35, 0, 250, 20),
@@ -19,7 +23,7 @@ def process_image(img: Image.Image, region: tuple[int, int, int, int]) -> np.nda
     img = extract_region(img, region)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     img = cv2.bitwise_not(img)
-    img = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_LINEAR)
+    img = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_LANCZOS4)
     _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return img
 
@@ -43,12 +47,14 @@ async def get_info(img: Image.Image) -> tuple[str, str]:
     return name, amount
 
 def fix_title(title: str):
+    title = title[0].upper() + title[1:].replace("V", "v")
     # Hide this from the light of DAY
     # title = text_striping(title)
     # title = title.replace("Oread", "Dread")
-    title = title.replace("Orone", "Drone")
+    # title = title.replace("Orone", "Drone")
+    # title = title.replace("Yexnium", "Vexnium")
     # title = title.replace("LGS", "LG5")
-    # title = title.replace("x9", "X9")
+    # # title = title.replace("x9", "X9")
     # title = re.sub(r'\s(?:I|T|L|f|l|1){1,3}(?=\s|$)',
     #                 lambda match: match.group(0)
     #                     .replace('T', 'I')
@@ -62,19 +68,19 @@ def fix_title(title: str):
 def preprocess_title(img: np.ndarray) -> np.ndarray:
     # img = cv2.resize(img, None, fx=1.2, fy=1, interpolation=cv2.INTER_CUBIC)
     # kern = np.ones((5, 5), np.uint8)
-    # img = cv2.GaussianBlur(img, (1, 1), 2)
+    img = cv2.GaussianBlur(img, (1, 1), 2)
 
-    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, np.ones((1, 1), np.uint8), iterations=1)
-    img = cv2.resize(img, None, fx=1.1, fy=1, interpolation=cv2.INTER_CUBIC)
-    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, np.ones((2, 2), np.uint8), iterations=1)
-    # img = cv2.dilate(img, np.ones((4, 1), np.uint8), iterations=1)
+    # img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, np.ones((1, 1), np.uint8), iterations=1)
+    # img = cv2.resize(img, None, fx=1.1, fy=1, interpolation=cv2.INTER_CUBIC)
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, np.ones((2, 2), np.uint8), iterations=1)
+    # img = cv2.dilate(img, np.ones((3, 2), np.uint8), iterations=1)
     # img = cv2.detailEnhance(img, sigma_s=10, sigma_r=0.15)
     # img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     # img = cv2.erode(img, np.ones((2, 0), np.uint8), iterations=1)
     return img
 
 async def get_title(img: np.ndarray):
-    img = preprocess_title(img)
+    # img = preprocess_title(img)
 
     buff = BytesIO()
     Image.fromarray(img).save(buff, format="PNG")
@@ -82,10 +88,10 @@ async def get_title(img: np.ndarray):
 
     output = await aiopytesseract.image_to_string(
         buff.getvalue(),
-        # lang="model",
-        psm=10,
+        # lang="starlstm_final",
         # oem=1,
-        # tessdata_dir="./tessdata",
+        psm=7,
+        tessdata_dir="./tessdata",
         config=[
             ("tessedit_char_blacklist", "$[]")
         ]
@@ -102,7 +108,7 @@ async def get_title(img: np.ndarray):
     return fix_title(output)
 
 def preprocess_amount(img: np.ndarray) -> np.ndarray:
-    img = cv2.resize(img, None, fx=2.1, fy=2, interpolation=cv2.INTER_CUBIC)
+    # img = cv2.resize(img, None, fx=2.1, fy=2, interpolation=cv2.INTER_CUBIC)
     # img = cv2.threshold(cv2.medianBlur(img, 1), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     # _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     # img = cv2.GaussianBlur(img, (3, 3), 2)
@@ -112,17 +118,17 @@ def preprocess_amount(img: np.ndarray) -> np.ndarray:
     #                [0, -1, 0]])
 
     # # Apply the kernel to the image
-    # # img = cv2.dilate(img, np.ones((3, 2), np.uint8), iterations=1)
-    # # _, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+    # img = cv2.dilate(img, np.ones((2, 2), np.uint8), iterations=1)
+    # _, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
     # img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8), iterations=1)
-    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, np.ones((3, 1), np.uint8), iterations=1)
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, np.ones((3, 1), np.uint8), iterations=1)
 
-    img = cv2.dilate(img, np.ones((0, 1), np.uint8), iterations=1)
+    # img = cv2.dilate(img, np.ones((0, 1), np.uint8), iterations=1)
     # img = cv2.filter2D(img, -1, kernel)
-    img = cv2.GaussianBlur(img, (5, 3), 0)
-    _, img = cv2.threshold(img, 40, 255, cv2.THRESH_BINARY)
-    img = cv2.dilate(img, np.ones((1, 2), np.uint8), iterations=1)
-    img = cv2.erode(img, np.ones((3, 3), np.uint8), iterations=1)
+    # img = cv2.GaussianBlur(img, (5, 3), 0)
+    # _, img = cv2.threshold(img, 40, 255, cv2.THRESH_BINARY)
+    # img = cv2.dilate(img, np.ones((1, 2), np.uint8), iterations=1)
+    # img = cv2.erode(img, np.ones((3, 3), np.uint8), iterations=1)
     return img
 
 async def get_amount(img: np.ndarray):
@@ -134,33 +140,14 @@ async def get_amount(img: np.ndarray):
 
     output = await aiopytesseract.image_to_string(
         buff.getvalue(),
-        # lang="model",
-        psm=10,
-        # oem=1,
-        # tessdata_dir="./tessdata",
+        lang="eng",
+        oem=3,
+        psm=7,
+        tessdata_dir="./tessdata",
         config=[
-            ("tessedit_char_blacklist", "$[]")
+            ("tessedit_char_whitelist", "0123456789ed")
         ]
     )
-
-    # output = pytesseract.image_to_string(img,
-    #     lang="model",
-    #     config="--psm 10"
-    #     " --oem 1"
-    #     ' -c tessedit_char_blacklist="$[]/"'
-    #     " --tessdata-dir ./tessdata"
-    #     )
-    # print(output.strip())
-    # if output.strip() == "":
-    #     print("Here")
-    #     output = pytesseract.image_to_string(img,
-    #         lang="eng",
-    #         config="--psm 10"
-    #         " --oem 1"
-    #         ' -c tessedit_char_blacklist="$[]/"'
-    #         " --tessdata-dir ./tessdata"
-    #         )
-    #     print(output.strip())
     if output.strip().endswith("ed"): return 0
     text = text_striping(output)
     # text = text.replace("?", "1").replace("s", "5").replace("S", "5").replace("g", "9").replace("]", "1")
